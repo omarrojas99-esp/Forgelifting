41f94f2bf2311f99462b0e84ce1d1b6e356bf85c
omarrojas99-esp <omarrojasm99@gmail.com>
2026-08-22 17:45:13 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,19 @@
-const CACHE_NAME = 'powerlifting-app-v1';
-const urlsToCache = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'forgelifting-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png'
+];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
